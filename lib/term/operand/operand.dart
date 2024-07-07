d3d5efc5e38250e0619c6b9ce234f0ab17983395
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 20:11:10 +0900

    FEAT: add value property to Operand class

--- lib/term/operand/operand.dart
+++ lib/term/operand/operand.dart
@@ -2,4 +2,7 @@ import 'package:console_calculator/term/term.dart';
 
 /// Contains a value used as input for operation
 class Operand extends Term {
+  final double value;
+
+  Operand(this.value);
 }
