ea7f82dd62f36966365aad459554aaf2bdaaa91d
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 23:31:02 +0900

    FEAT: create PostfixExpressionEvaluator class

---
+++ lib/expression_evaluator/postfix_expression_evaluator.dart
@@ -0,0 +1,11 @@
+import 'package:console_calculator/expression_evaluator/expression_evaluator.dart';
+import 'package:console_calculator/term/term.dart';
+
+/// Evaluate a expression using postfix expression algorithm
+class PostfixExpressionEvaluator implements ExpressionEvaluator {
+  @override
+  double evaluate(List<Term> expressionTokens) {
+    // TODO: implement evaluate
+    throw UnimplementedError();
+  }
+}
