0eca681e6ea6ba5453ec612943b51977e89eb4ca
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 18:48:42 +0900

    FEAT: add ExpressionEvaluator, Tokenizer properties to BasicCalculator

--- lib/calculator/basic_calculator.dart
+++ lib/calculator/basic_calculator.dart
@@ -1,6 +1,16 @@
 import 'package:console_calculator/calculator/calculator.dart';
+import 'package:console_calculator/expression_evaluator/expression_evaluator.dart';
+import 'package:console_calculator/util/tokenizer/tokenizer.dart';
 
+// Basic calculator perform calculate using ExpressionEvaluator and Tokenizer
 class BasicCalculator implements Calculator {
+  final ExpressionEvaluator _expressionEvaluator;
+  final Tokenizer _tokenizer;
+
+  BasicCalculator({required expressionEvaluator, required tokenizer})
+      : _expressionEvaluator = expressionEvaluator,
+        _tokenizer = tokenizer;
+
   @override
   String calculate(List<String> expression) {
     return '';
