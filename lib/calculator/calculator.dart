e13ae508f008feba34f90acaf82599fb7f2b1efd
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 17:45:32 +0900

    FEAT: create Calculator abstract class

---
+++ lib/calculator/calculator.dart
@@ -0,0 +1,5 @@
+/// Main calculator that performs calculations on input
+abstract interface class Calculator {
+  /// Calculate result of given expression
+  String calculate(List<String> expression);
+}
