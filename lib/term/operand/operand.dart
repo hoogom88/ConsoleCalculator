3c8e786df3e5eff9f57a77f57402b0b6f57a7cb2
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 23:23:03 +0900

    FEAT: add equal operator to Operand class

--- lib/term/operand/operand.dart
+++ lib/term/operand/operand.dart
@@ -5,4 +5,16 @@ class Operand extends Term {
   final double value;
 
   Operand(this.value);
+
+  // implements by generator
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Operand &&
+          runtimeType == other.runtimeType &&
+          value == other.value;
+
+  // implements by generator
+  @override
+  int get hashCode => value.hashCode;
 }
