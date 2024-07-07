1041921ab88e18808bd2f5921c34296d09368769
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 17:46:31 +0900

    FEAT: create BasicCalculator class

---
+++ lib/calculator/basic_calculator.dart
@@ -0,0 +1,8 @@
+import 'package:console_calculator/calculator/calculator.dart';
+
+class BasicCalculator implements Calculator {
+  @override
+  String calculate(List<String> expression) {
+    return '';
+  }
+}
