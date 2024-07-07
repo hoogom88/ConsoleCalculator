83715f9c6001eda8d649f1416b0b7a5b948bc1b5
hoogom88 <ziziwhdgus@naver.com>
2024-07-08 03:01:21 +0900

    FEAT: create BasicExpressionValidator

---
+++ lib/expression_validator/basic_expression_validator.dart
@@ -0,0 +1,11 @@
+import 'package:console_calculator/expression_validator/expression_validator.dart';
+import 'package:console_calculator/term/term.dart';
+
+class BasicExpressionValidator implements ExpressionValidator {
+  @override
+  bool validate(List<Term> expressions) {
+    // TODO: implement validate
+    throw UnimplementedError();
+  }
+
+}
