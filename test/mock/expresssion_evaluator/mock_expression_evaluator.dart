05c8ac88b3240838a1ded8f66c875f3af5ae1afd
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 19:13:17 +0900

    TEST: create MockExpressionEvaluator, MockTokenizer class

---
+++ test/mock/expresssion_evaluator/mock_expression_evaluator.dart
@@ -0,0 +1,21 @@
+import 'package:console_calculator/expression_evaluator/expression_evaluator.dart';
+import 'package:console_calculator/term/term.dart';
+
+class MockExpressionEvaluator implements ExpressionEvaluator {
+  int evaluateCallCount = 0;
+  List<Term> evaluateParameter = [];
+  double evaluateReturnValue = 0;
+
+  void resetMockData() {
+    evaluateCallCount = 0;
+    evaluateParameter.clear();
+    evaluateReturnValue = 0;
+  }
+
+  @override
+  double evaluate(List<Term> expressionTokens) {
+    evaluateCallCount++;
+    evaluateParameter = expressionTokens;
+    return evaluateReturnValue;
+  }
+}
---
+++ test/mock/util/tokenizer/mock_tokenizer.dart
@@ -0,0 +1,21 @@
+import 'package:console_calculator/term/term.dart';
+import 'package:console_calculator/util/tokenizer/tokenizer.dart';
+
+class MockTokenizer implements Tokenizer {
+  int tokenizeCallCount = 0;
+  List<String> tokenizeParameter = [];
+  List<Term> tokenizeReturnValue = [];
+
+  void resetMockData() {
+    tokenizeCallCount = 0;
+    tokenizeParameter.clear();
+    tokenizeReturnValue.clear();
+  }
+
+  @override
+  List<Term> tokenize(List<String> expression) {
+    tokenizeCallCount++;
+    tokenizeParameter = expression;
+    return tokenizeReturnValue;
+  }
+}
