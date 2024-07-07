4da9ed8e705eb39a77aeaa9f3c4fa811d73e0628
hoogom88 <ziziwhdgus@naver.com>
2024-07-08 01:01:20 +0900

    FEAT: implement PrecedenceResolver method (passed test)

--- lib/util/precedence_resolver/precedence_resolver.dart
+++ lib/util/precedence_resolver/precedence_resolver.dart
@@ -1,11 +1,21 @@
+import 'package:console_calculator/term/operation/addition.dart';
+import 'package:console_calculator/term/operation/division.dart';
+import 'package:console_calculator/term/operation/multiplication.dart';
 import 'package:console_calculator/term/operation/operation.dart';
+import 'package:console_calculator/term/operation/subtraction.dart';
+import 'package:console_calculator/util/exception.dart';
 
 /// Provide precedence of operation
 class PrecedenceResolver {
-  static final Map<Operation, int> _precedence = {};
 
   /// Returns precedence of given Operation
   static int precedence(Operation operation) {
-    return 0;
+    return switch(operation) {
+      Addition _ => 0,
+      Subtraction _ => 0,
+      Division _ => 1,
+      Multiplication _ => 1,
+      _ => throw SimpleBusinessException.invalidInput()
+    };
   }
 }
