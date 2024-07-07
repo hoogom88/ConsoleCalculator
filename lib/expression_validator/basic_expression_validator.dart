064c94e5ab0a5983df6cf965723d0b2cb120774c
hoogom88 <ziziwhdgus@naver.com>
2024-07-08 04:08:23 +0900

    FEAT: implement BasicExpressionValidator method (passed test)

--- lib/expression_validator/basic_expression_validator.dart
+++ lib/expression_validator/basic_expression_validator.dart
@@ -1,11 +1,13 @@
 import 'package:console_calculator/expression_validator/expression_validator.dart';
+import 'package:console_calculator/term/operand/operand.dart';
+import 'package:console_calculator/term/operation/operation.dart';
 import 'package:console_calculator/term/term.dart';
 
 /// Validate a expression based on four fundamental arithmetic operations(+, -, /, *)
+/// (only [Operand, Operation, Operand] is allow)
 class BasicExpressionValidator implements ExpressionValidator {
   @override
   bool validate(List<Term> expressions) {
-    // TODO: implement validate
-    throw UnimplementedError();
+    return (expressions.length == 3 && expressions[0] is Operand && expressions[1] is Operation && expressions[2] is Operand);
   }
 }
