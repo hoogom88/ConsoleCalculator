75cad7a11f0f18d91d0337530dcf9288fffbf0e3
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 19:24:27 +0900

    FEAT: create DefaultTokenizer class

---
+++ lib/util/tokenizer/default_tokenizer.dart
@@ -0,0 +1,9 @@
+import 'package:console_calculator/term/term.dart';
+import 'package:console_calculator/util/tokenizer/tokenizer.dart';
+
+class DefaultTokenizer implements Tokenizer {
+  @override
+  List<Term> tokenize(List<String> expression) {
+    return [];
+  }
+}
