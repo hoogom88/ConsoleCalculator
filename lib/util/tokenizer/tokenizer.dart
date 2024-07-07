fa16110919dbaf52682b8c8b9b8fb1e49e0a5f22
hoogom88 <ziziwhdgus@naver.com>
2024-07-07 18:35:33 +0900

    FEAT: create Tokenizer interface

---
+++ lib/util/tokenizer/tokenizer.dart
@@ -0,0 +1,7 @@
+import 'package:console_calculator/term/term.dart';
+
+/// Breaks expression into Term
+abstract interface class Tokenizer {
+  /// Returns separated Terms from given expression
+  List<Term> tokenize(String expression);
+}
