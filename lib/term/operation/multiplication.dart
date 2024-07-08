6f9d9d46a6ea6a179c5238d7a62f40fb2910b710
hoogom88 <ziziwhdgus@naver.com>
2024-07-08 12:57:17 +0900

    FEAT: implements Multiplication/Subtraction.execute() (passed test)

--- lib/term/operation/multiplication.dart
+++ lib/term/operation/multiplication.dart
@@ -1,5 +1,7 @@
+import 'package:console_calculator/term/operand/number.dart';
 import 'package:console_calculator/term/operand/operand.dart';
 import 'package:console_calculator/term/operation/operation.dart';
+import 'package:console_calculator/util/exception.dart';
 
 /// Perform multiplication with two Operands
 /// Singleton
@@ -18,7 +20,10 @@ class Multiplication extends Operation {
 
   @override
   Operand execute(List<Operand> operands) {
-    // TODO: implement execute
-    throw UnimplementedError();
+    try {
+      return Number((double.parse(operands[0].value) * double.parse(operands[1].value)).toString());
+    } catch (e) {
+      throw SimpleBusinessException.overflow();
+    }
   }
 }
